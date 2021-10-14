61d51b1b05bda8f93c0e4a983f295dbfc2b3c20e
russuvlad97 <russu.vlad97@gmail.com>
2021-10-14 19:05:00 +0300

    Add project files.

---
+++ russu_vlad_lab2/Data/LibraryContext.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using russu_vlad_lab2.Models;
+
+namespace russu_vlad_lab2.Data
+{
+    public class LibraryContext:DbContext
+    {
+        /*public LibraryContext()
+        {
+        }*/
+
+        public LibraryContext(DbContextOptions<LibraryContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Customer>().ToTable("Customer");
+            modelBuilder.Entity<Order>().ToTable("Order");
+            modelBuilder.Entity<Book>().ToTable("Book");
+        }
+    }
+}
---
+++ russu_vlad_lab2/Models/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace russu_vlad_lab2.Models
+{
+    public class Customer
+    {
+        [DatabaseGenerated(DatabaseGeneratedOption.None)]
+            public int CustomerID { get; set; }
+            public string Name { get; set; }
+            public string Address { get; set; }
+            public DateTime BirthDate { get; set; }
+
+        public ICollection<Order> Orders { get; set; }
+     }
+}
